2535fa33f56d70578e1e56de6051a9dbc098b0eb
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-06-06 23:44:30 +0800

    Add react query in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,21 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 const container = document.getElementById('root')!;
 const root = createRoot(container); 
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
