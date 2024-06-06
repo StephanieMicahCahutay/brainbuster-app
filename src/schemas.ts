1e4f9037e1a6fd352fb10d96099d3ce83c869787
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-06-07 00:03:14 +0800

    Add schema for signup

--- src/schemas.ts
+++ src/schemas.ts
@@ -6,3 +6,14 @@ export const loginSchema = z.object({
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
+
+export const signupSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(6, { message: 'Password must be at least 6 characters' }),
+  confirmPassword: z.string().min(6, { message: 'Password must be at least 6 characters' }),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: 'Passwords must match',
+  path: ['confirmPassword'], // field path
+});
+
+export type SignupSchema = z.infer<typeof signupSchema>;
